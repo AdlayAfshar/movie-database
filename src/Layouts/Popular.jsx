7a92277de9acb1e72de27467201fe15ad2062e4f
Yalda Afshar <adlayafshar@gmail.com>
2023-04-25 18:36:15 +0100

    Add popular section

---
+++ src/Layouts/Popular.jsx
@@ -0,0 +1,14 @@
+import './popular.scss';
+
+export const Popular = () => {
+    return (
+        <div className="popular">
+           <h2>popular</h2> 
+            <p> Millions of movies, TV shows and people to discover. Explore now.</p>
+            <p> Millions of movies, TV shows and people to discover. Explore now.</p>
+            <p> Millions of movies, TV shows and people to discover. Explore now.</p>
+            <p> Millions of movies, TV shows and people to discover. Explore now.</p>
+        
+        </div>
+    )
+}
