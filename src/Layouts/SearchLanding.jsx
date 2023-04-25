6f105c609d3970d225770f0fb5f3f49248f1ba35
Yalda Afshar <adlayafshar@gmail.com>
2023-04-25 18:35:53 +0100

    Add search section

---
+++ src/Layouts/SearchLanding.jsx
@@ -0,0 +1,11 @@
+import './searchLanding.scss';
+
+export const SearchLanding = () => {
+    return (
+        <div className="searchLanding">
+           <h2>SEARCh</h2> 
+            <p> Millions of movies, TV shows and people to discover. Explore now.</p>
+            <input type="text" />
+        </div>
+    )
+}
