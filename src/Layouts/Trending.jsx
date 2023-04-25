3086adcf3cadffc146169d5e8438d33151427e0d
Yalda Afshar <adlayafshar@gmail.com>
2023-04-25 20:37:42 +0100

    Trending with time

--- src/Layouts/Trending.jsx
+++ src/Layouts/Trending.jsx
@@ -3,23 +3,21 @@ import { useState, useEffect } from "react";
 
 export const Trending = ({ className }) => {
     const media = ["all", "movie", "tv"];
-    const time = ["day", "week"];
+    const [time, setTime] = useState("day");
     const baseUrl = "https://api.themoviedb.org/3/";
     const apiKey = "ffa300523873658c0dc98283306a3c45";
     const requestParams = `?api_key=${apiKey}`;
 
-
     const [results, setResults] = useState(null);
 
     const fetchData = async () => {
-        const urlToFetch = `${baseUrl}/trending/${media[0]}/${time[1]}${requestParams}`;
+        const urlToFetch = `${baseUrl}/trending/${media[0]}/${time}${requestParams}`;
         try {
             const response = await fetch(urlToFetch);
 
             if (response.ok) {
                 const jsonResponse = await response.json();
                 const results = jsonResponse.results;
-                // console.log({ results });
                 setResults(results);
                 return results;
             }
@@ -28,16 +26,16 @@ export const Trending = ({ className }) => {
         }
     };
 
-
     useEffect(() => {
         fetchData();
-    });
+    }, [time]);
 
+    const handelClick = (e) => {
+        e.target.dataset.id === "week" ? setTime("day") : setTime("week");
+    };
 
     console.log({ results });
     const img = `https://image.tmdb.org/t/p/w440_and_h660_face/`;
-
-
 
     return (
         <div className="trending className">
@@ -45,42 +43,42 @@ export const Trending = ({ className }) => {
                 <h2 className="trending-header__title">Trending</h2>
                 <div className="trending-header__selector">
                     <h3>
-                        {/* <a>Today</a> */}
-                        Today
+                        <button data-id='day' onClick={handelClick}>
+                            today
+                        </button>      
                     </h3>
 
                     <h3>
-                        {/* <a>This Week</a> */}
-                        This Week
+                        <button data-id='week' onClick={handelClick}>
+                            this week
+                        </button>
                     </h3>
                 </div>
             </div>
 
             <div className="trending-content">
-            {results?.map((user) => (
-                <div className="card">
-                    <div className="card-img">
-                        <a href="https://www.themoviedb.org/tv/82856-the-mandalorian">
-                            <img
-                                src={img + user.poster_path}
-                                alt={user.title}
-                            />
-                        </a>
-                    </div>
-
-                    <div className="card-content">
-                        <div className="card-content__rate">{user.vote_average.toFixed(1)}</div>
-                        <h2>
+                {results?.map((user) => (
+                    <div className="card">
+                        <div className="card-img">
                             <a href="https://www.themoviedb.org/tv/82856-the-mandalorian">
-                            {user.title}
+                                <img src={img + user.poster_path} alt={user.title} />
                             </a>
-                        </h2>
-                        <p> {user.release_date} </p>
+                        </div>
+
+                        <div className="card-content">
+                            <div className="card-content__rate">
+                                {user.vote_average.toFixed(1)}
+                            </div>
+                            <h2>
+                                <a href="https://www.themoviedb.org/tv/82856-the-mandalorian">
+                                    {user.title}
+                                </a>
+                            </h2>
+                            <p> {user.release_date} </p>
+                        </div>
                     </div>
-                </div>
                 ))}
             </div>
-
         </div>
     );
 };
