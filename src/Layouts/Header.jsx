4516a643975587456d3a26090744a06a1fb14b5d
Yalda Afshar <adlayafshar@gmail.com>
2023-04-25 18:36:52 +0100

    Add header section

---
+++ src/Layouts/Header.jsx
@@ -0,0 +1,23 @@
+import './header.scss';
+// import { Main } from '../Pages/Main'
+// import { Movies } from '../Pages/Movies'
+// import { TVshows } from '../Pages/TVshows'
+// import { Search } from '../Components/Search'
+
+
+export function Header() {
+
+  return (
+    <div className="header">
+      {/* <Main /> */}
+      {/* <Movies />
+      <TVshows />  
+      <Search /> */}
+      <div> main </div>
+      <div> Movies </div>
+      <div> TVshows </div>
+      <input type="search" />
+      
+    </div>
+  );
+}
