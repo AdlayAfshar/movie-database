62232fa6d2ecce83451199bd039f903dbbfe6392
Yalda Afshar <adlayafshar@gmail.com>
2023-05-04 23:13:27 +0100

    edit

--- src/Layouts/SortBox.jsx
+++ src/Layouts/SortBox.jsx
@@ -5,7 +5,7 @@ export function SortBox({ value, onChange }) {
 
     return (
         <div className="sortBox">
-            <h3 className="sortBox__label">Sort Results By</h3>
+            <h3>Sort Results By</h3>
 
             <SortResults
                 className="sortBox__select"
