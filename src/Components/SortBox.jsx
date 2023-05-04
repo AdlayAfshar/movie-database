b4118bf1b634e631edca530ab76cdafbc1be833e
Yalda Afshar <adlayafshar@gmail.com>
2023-05-04 23:35:59 +0100

    move

---
+++ src/Components/SortBox.jsx
@@ -0,0 +1,16 @@
+import "./sortBox.scss";
+import { SortResults } from "./SortResults";
+
+export function SortBox({ value, onChange }) {
+  return (
+    <div className="sortBox">
+      <h3>Sort Results By</h3>
+
+      <SortResults
+        className="sortBox__select"
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+}
