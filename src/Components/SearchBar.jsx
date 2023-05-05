970a4299cadf5e05e6be0a508f58dfe34bca0982
Yalda Afshar <adlayafshar@gmail.com>
2023-05-05 02:26:51 +0100

    refactor

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,20 +1,20 @@
 // import "./searchBar.scss";
 
 export function SearchBar({ onSubmit, onChange, searchKey }) {
-    return (
-        <div className="searchBar">
-            <form className='form' onSubmit={onSubmit}>
+  return (
+    <div className="searchBar">
+      <form className="form" onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          value={searchKey}
+          className="form__input"
+        />
 
-                <input type='text' onChange={onChange} value={searchKey} className='form__input' />
-
-                <div className='form__btn-group'>
-
-                    <button className='form__btn'> Search </button>
-
-                </div>
-
-            </form>
-
+        <div className="form__btn-group">
+          <button className="form__btn"> Search </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
--- src/Layouts/SearchLanding.jsx
+++ src/Layouts/SearchLanding.jsx
@@ -1,52 +1,60 @@
-import './searchLanding.scss';
+import "./searchLanding.scss";
 import { useState } from "react";
 import { fetchData } from "../helper/fetchData";
-import { ShowMedia } from '../Components/ShowMedia';
-import { SearchBar } from '../Components/SearchBar';
+import { ShowMedia } from "../Components/ShowMedia";
+import { SearchBar } from "../Components/SearchBar";
 
 export const SearchLanding = ({ baseUrl, requestParams }) => {
+  const [searchKey, setSearchKey] = useState("");
+  const [results, setResults] = useState([]);
 
-    const [searchKey, setSearchKey] = useState('');
-    const [results, setResults] = useState([]);
+  const fetchSearchMovie = async (searchKey) => {
+    const urlToFetch = `${baseUrl}search/movie${requestParams}&query=${searchKey}`;
+    const data = await fetchData(urlToFetch);
+    setResults((results) => [...results, ...data.results]);
+  };
 
-    const fetchSearchMovie = async (searchKey) => {
-        const urlToFetch = `${baseUrl}search/movie${requestParams}&query=${searchKey}`;
-        const results = await fetchData(urlToFetch)
-        setResults([...results.results]);
-    }
+  const fetchSearchTv = async (searchKey) => {
+    const urlToFetch = `${baseUrl}search/tv${requestParams}&query=${searchKey}`;
+    const data = await fetchData(urlToFetch);
+    setResults((results) => [...results, ...data.results]);
+  };
 
-    const fetchSearchTv = async (searchKey) => {
-        const urlToFetch = `${baseUrl}search/tv${requestParams}&query=${searchKey}`;
-        const results = await fetchData(urlToFetch)
-        setResults([...results.results]);
-    }
+  const handleChange = ({ target }) => {
+    setSearchKey(target.value);
+  };
 
-    const handleChange = ({ target }) => {
-        setSearchKey(target.value);
-    }
+  const searchMovie = (e) => {
+    //prevents page refresh
+    setResults([]);
+    e.preventDefault();
+    fetchSearchMovie(searchKey);
+    fetchSearchTv(searchKey);
+    setSearchKey("");
+  };
 
+  return (
+    <div className="searchLanding">
+      <div className="searchLanding-title">
+        <h2>SEARCh.</h2>
+        <h3>
+          {" "}
+          Millions of movies, TV shows and people to discover. Explore now.
+        </h3>
+      </div>
 
+      <SearchBar
+        type="text"
+        onSubmit={searchMovie}
+        onChange={handleChange}
+        searchKey={searchKey}
+      />
 
-    const searchMovie = (e) => {
-        //prevents page refresh
-        e.preventDefault();
-        fetchSearchMovie(searchKey);
-        fetchSearchTv(searchKey);
-        setSearchKey('');
-
-    }
-
-    return (
-        <div className="searchLanding">
-            <div className="searchLanding-title">
-                <h2>SEARCh.</h2>
-                <h3> Millions of movies, TV shows and people to discover. Explore now.</h3>
-            </div>
-
-            <SearchBar type='text' onSubmit={searchMovie} onChange={handleChange} searchKey={searchKey} />
-    
-            <ShowMedia media={results} path={results.media_type} className="searchLanding-card" />
-
-        </div>
-    )
-}
+      <ShowMedia
+        media={results}
+        path={results.media_type}
+        className="searchLanding-card"
+      />
+    </div>
+  );
+};
